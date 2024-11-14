29878c10fc16185c437f2ffb6a7c2da6e834cbf6
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-11-14 17:16:31 -0300

    playsinline

--- app/components/VideoSection/VideoSection.jsx
+++ app/components/VideoSection/VideoSection.jsx
@@ -45,7 +45,7 @@ export default function VideoSection() {
             ref={videoRef[0]}
             controls={isVideoPlaying ? true : false}
             src="https://video.wixstatic.com/video/113b92_ff87c096baf1408995c1596e4f5936a5/480p/mp4/file.mp4"
-    
+            playsInline
             preload="auto"
             style={{ width: '300px', height: '300px' }}
           />
@@ -59,8 +59,9 @@ export default function VideoSection() {
         <div className={c.item}>
           <video
             ref={videoRef[1]}
-            
             src="https://video.wixstatic.com/video/113b92_16e42514afb04c34882f61b365c417c0/480p/mp4/file.mp4"
+            controls={isVideoPlaying ? true : false}
+            playsInline
             preload="auto"
             style={{ width: '300px', height: 'auto' }}
           />
