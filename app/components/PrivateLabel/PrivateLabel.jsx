16471250b71e068bfdd2d8b838948315d61afdf6
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-11-21 17:22:54 -0300

    vsfd gabriel

--- app/components/PrivateLabel/PrivateLabel.jsx
+++ app/components/PrivateLabel/PrivateLabel.jsx
@@ -2,11 +2,21 @@ import c from './PrivateLabel.module.css';
 
 export default function PrivateLabel() {
   return (
-    <div className={c.cont} id='privatelabel'>
+    <div className={c.cont} id="privatelabel">
       <img src="/17871565.webp" alt="private label" />
-      <p>
-      Nossa divisão Private Label está focada em gerar valor para marcas do universo fashion que desejam agregar bolsas e acessórios ao seu mix ou que buscam oferecer estes produtos com maior competitividade no mercado. Para saber como a GO Bags pode ajudar sua marca, entre em contato conosco e que um de nossos executivos de atendimento irá lhe atender <span><a href='mailto:privatelabel@gohb.com.br'>privatelabel@gohb.com.br</a></span> 
-      </p>
+      <div className={c.pragraphWrapper}>
+        <div className={c.paragraph}>
+          Nossa divisão Private Label está focada em gerar valor para marcas do
+          universo fashion que desejam agregar bolsas e acessórios ao seu mix ou
+          que buscam oferecer estes produtos com maior competitividade no mercado.
+          Para saber como a GO Bags pode ajudar sua marca, entre em contato
+          conosco e que um de nossos executivos de atendimento irá lhe atender{' '}
+          <span>
+            <a href="mailto:privatelabel@gohb.com.br">privatelabel@gohb.com.br</a>
+          </span>
+        </div>
+        
+      </div>
     </div>
-  )
+  );
 }
